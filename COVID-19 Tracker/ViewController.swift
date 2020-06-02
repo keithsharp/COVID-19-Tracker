b32efc3430b1ea6fc0d953042e17a5964b5872e3
Keith Sharp <kms@passback.co.uk>
2020-06-02 13:25:22 +0100

    Initial Commit

---
+++ COVID-19 Tracker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  COVID-19 Tracker
+//
+//  Created by Keith Sharp on 02/06/2020.
+//  Copyright © 2020 Keith Sharp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
