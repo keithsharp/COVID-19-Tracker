152657b67ddf549255302d8d752ae783eaa64e36
Keith Sharp <kms@passback.co.uk>
2020-06-21 16:54:42 +0100

    Added a custom WindowController Set the window title Save the size and position of the window

---
+++ COVID-19 Tracker/MainWindowController.swift
@@ -0,0 +1,20 @@
+//
+//  MainWindowController.swift
+//  COVID-19 Tracker
+//
+//  Created by Keith Sharp on 21/06/2020.
+//  Copyright © 2020 Keith Sharp. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        window?.title = "United Kingdom COVID-19 Deaths"
+        self.windowFrameAutosaveName = "position"
+    }
+
+}
